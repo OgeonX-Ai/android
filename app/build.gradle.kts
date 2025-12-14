54f34d18b6eef954e9acc458b32cef0c9e996d7a
Kim Harjamaki <ogeonx@gmail.com>
2025-12-14 15:44:40 +0200

    Add Material Components dependency for XML theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("com.google.android.material:material:1.12.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
